49ec6279d625f886caabcc6ee86542799f2e3c1a
gaurav kanthed <kanthedgaurav@gmail.com>
2024-09-30 18:49:44 -0500

    fifth commit

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -4,15 +4,16 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from db.repository.blog import list_blogs , retrieve_blog
 from db.session import get_db
+from typing import Optional
 
 templates = Jinja2Templates(directory="templates")
 
 router = APIRouter()
 
 @router.get("/")
-def home(request: Request, db: Session = Depends(get_db)):
+def home(request: Request, alert: Optional[str] = None, db: Session = Depends(get_db)):
     blogs = list_blogs(db)
-    context = {"request": request, "blogs": blogs}
+    context = {"request": request, "blogs": blogs,"alert": alert}
     return templates.TemplateResponse("blogs/home.html", context= context)
 
 @router.get("/app/blog/{blog_id}")
@@ -20,3 +21,4 @@ def blog_details(blog_id: int, request: Request, db: Session = Depends(get_db)):
     blog = retrieve_blog(blog_id, db)
     context = {"request": request, "blog": blog}
     return templates.TemplateResponse("blogs/detail.html", context= context)
+
--- backend/apps/v1/route_login.py
+++ backend/apps/v1/route_login.py
@@ -8,6 +8,9 @@ from sqlalchemy.orm import session
 from db.session import get_db
 from schemas.user import UserCreate
 from db.repository.user import create_new_user
+from apis.v1.route_login import authenticate_user
+from core.security import create_access_token
+
 
 templates = Jinja2Templates(directory="templates")
 
@@ -23,9 +26,25 @@ def resgister(request: Request,  email: str = Form(...), password: str = Form(..
     try:
         user = UserCreate( email=email, password=password)
         create_new_user(user, db)
-        return responses.RedirectResponse(url="/?msg=Successfully Registered", status_code=status.HTTP_302_FOUND)
+        return responses.RedirectResponse(url="/?alert=Successfully%20Registered", status_code=status.HTTP_302_FOUND)
     except Exception as e:
         erros_list = json.loads(e.json())
         for item in erros_list:
             errors.append(item.get("loc")[0] +": "+ item.get("msg"))
         return templates.TemplateResponse("auth/register.html", {"request": request, "errors": errors, "email": email})
+    
+@router.get("/login")
+def login(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
+
+@router.post("/login")
+def login(request: Request, email: str = Form(...), password: str = Form(...), db: session = Depends(get_db)):
+    errors = []
+    user = authenticate_user(email=email, password=password, db=db)
+    if not user:
+        errors.append("Incorrect Email or Password")
+        return templates.TemplateResponse("auth/login.html", {"request": request, "errors": errors, "email": email})
+    access_token = create_access_token(data={"sub": email})
+    response = responses.RedirectResponse(url="/?alert=Successfully%20Logged%20In", status_code=status.HTTP_302_FOUND)
+    response.set_cookie(key="access_token", value=f"Bearer {access_token}")
+    return response
