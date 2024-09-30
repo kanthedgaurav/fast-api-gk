791f1a5872860c7c7da6681baa48d6737835105c
gaurav kanthed <kanthedgaurav@gmail.com>
2024-09-29 23:46:40 -0500

    third committed

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -1,12 +1,22 @@
 from fastapi import APIRouter
-from fastapi import Request
+from fastapi import Request, Depends
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+from db.repository.blog import list_blogs , retrieve_blog
+from db.session import get_db
 
 templates = Jinja2Templates(directory="templates")
 
 router = APIRouter()
 
 @router.get("/")
-def home(request: Request):
-    print(dir(request))
-    return templates.TemplateResponse("blogs/home.html", {"request": request})
+def home(request: Request, db: Session = Depends(get_db)):
+    blogs = list_blogs(db)
+    context = {"request": request, "blogs": blogs}
+    return templates.TemplateResponse("blogs/home.html", context= context)
+
+@router.get("/app/blog/{blog_id}")
+def blog_details(blog_id: int, request: Request, db: Session = Depends(get_db)):
+    blog = retrieve_blog(blog_id, db)
+    context = {"request": request, "blog": blog}
+    return templates.TemplateResponse("blogs/detail.html", context= context)
